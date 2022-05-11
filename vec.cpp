bc7fea5be34112042171a779eb5042900496eedf
alex <alebo62@yandex.ru>
2022-05-11 17:48:21 +0300

    11.05 work4

--- vec.cpp
+++ vec.cpp
@@ -4,3 +4,13 @@ Vec::Vec()
 {
 
 }
+
+template<class T>
+Vec<T>& Vec<T>::operator =(const Vec &v)
+{
+    if(this == &v)
+        return *this;
+    uncreate();
+    create(v.begin(), v.end());
+    return *this;
+}
--- vec.h
+++ vec.h
@@ -5,12 +5,32 @@ template<class T>
 class Vec
 {
 public:
+    typedef T* iterator;
+    typedef const T* const_iterator;
+    typedef const T& const_reference;
+    typedef T& reference;
+    typedef std::size_t size_type;
+    typedef T value_type;
+
     Vec() { create();}
-    explicit Vec(std::size_t sz, const T& val = T())    { create(sz, val);}
+    explicit Vec(size_type, const T& val = T())    { create(sz, val);}
+    Vec(const Vec& v) { create(v.begin(),v.end());}
+    Vec& operator =(const Vec& v);
 private:
    T* data;
    T* limit;
    //T* array;
+public:
+   iterator begin() {return data;}
+   const_iterator begin() const {return data;}
+
+   iterator end() { return limit;}
+   const_iterator end() const { return limit;}
+
+   size_type size() const {return limit - data;}
+   reference operator[](size_type st){return data[i];}
+   const_reference operator[](size_type st){return data[i];}
+
 };
 
 #endif // VEC_H
