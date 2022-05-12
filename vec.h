8e3418905c7317e492944da9c6a680d09056af82
Alex <alebo62@inbox.ru>
2022-05-12 09:12:04 +0300

    12.05 utro commit

--- vec.h
+++ vec.h
@@ -1,6 +1,6 @@
 #ifndef VEC_H
 #define VEC_H
-
+#include <memory>
 template<class T>
 class Vec
 {
@@ -16,21 +16,38 @@ public:
     explicit Vec(size_type, const T& val = T())    { create(sz, val);}
     Vec(const Vec& v) { create(v.begin(),v.end());}
     Vec& operator =(const Vec& v);
+    ~Vec(){ uncreate();}
 private:
-   T* data;
-   T* limit;
-   //T* array;
+    T* data;
+    T* limit;
+    T* avail;
+    std::allocator<T> alloc;
+
+    void create();
+    void create(size_type, const T&);
+    void create(const_iterator, const_iterator);
+
+    void uncreate();
+
+    void grow();
+    void unchecked_append(const T&);
 public:
-   iterator begin() {return data;}
-   const_iterator begin() const {return data;}
+    iterator begin() {return data;}
+    const_iterator begin() const {return data;}
 
-   iterator end() { return limit;}
-   const_iterator end() const { return limit;}
+    iterator end() { return avail;}
+    const_iterator end() const { return avail;}
 
-   size_type size() const {return limit - data;}
-   reference operator[](size_type st){return data[i];}
-   const_reference operator[](size_type st){return data[i];}
 
+    size_type size() const {return avail - data;}
+    reference operator[](size_type st){return data[i];}
+    const_reference operator[](size_type st){return data[i];}
+
+    void push_back(const T& val){
+        if(avail == limit)
+            grow();
+        unchecked_append(val);
+    }
 };
 
 #endif // VEC_H
